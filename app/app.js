8c0916cd5a5bc1a6555730287463dad83134039d
khepin <khepin@gmail.com>
2013-12-03 10:35:47 +0800

    add login

--- app/app.js
+++ app/app.js
@@ -6,6 +6,8 @@ var app = express();
 
 app.use(express.bodyParser());
 app.use(express.static('frontend'));
+app.use(express.cookieParser('keyboard cat'));
+app.use(express.session());
 var db = mongoskin.db('localhost:27017/test', {safe:true});
 
 app.param('collectionName', function(req, res, next, collectionName){
@@ -15,6 +17,34 @@ app.param('collectionName', function(req, res, next, collectionName){
 
 app.get('/', function(req, res) {
     res.send('please select a collection, e.g., /api/messages');
+});
+
+app.post('/login', function(req, res){
+    var collection = db.collection('users');
+    var params = {username: req.body.username};
+    collection.findOne(params, function(e, result) {
+        if (e) return next(e);
+        if (!result) {
+            res.status(401);
+            res.send();
+        } else {
+            req.session.user = result;
+            res.send(result);
+        }
+    });
+});
+
+app.get('/login', function(req, res) {
+    if (req.session.user) {
+        res.send(req.session.user);
+    } else {
+        res.status(401).send();
+    }
+});
+
+app.post('/logout', function(req, res){
+    delete req.session.user;
+    res.status(204).send();
 });
 
 app.get('/api/:collectionName', function(req, res) {
